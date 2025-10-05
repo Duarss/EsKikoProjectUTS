c40a31ae76a724482f6dd27ba12a557ac97df9d8
Mathew Reynald <mathew.reynald04@gmail.com>
2025-10-05 21:06:52 +0700

    drawer dan nav bar

--- app/src/main/java/com/example/eskikoprojectuts/view/MainActivity.kt
+++ app/src/main/java/com/example/eskikoprojectuts/view/MainActivity.kt
@@ -5,17 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.eskikoprojectuts.R
+import com.example.eskikoprojectuts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view) // change the argument of setContentView
+
+        navController = (supportFragmentManager.findFragmentById(R.id.navHost)
+                as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        // setup drawer
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        // bottom bar
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+                || super.onSupportNavigateUp()
     }
 }
