7701e92f8b5c2302dd1f806ac14315345bbba374
Mathew Reynald <mathew.reynald04@gmail.com>
2025-10-06 15:51:20 +0700

    Edit Model Anak

--- app/src/main/java/com/example/eskikoprojectuts/model/Anak.kt
+++ app/src/main/java/com/example/eskikoprojectuts/model/Anak.kt
@@ -1,10 +1,12 @@
 package com.example.eskikoprojectuts.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Anak (
-    var id: String?,
-    var name: String?,
-    var bod: String?,
-    var sex: String?,
+    @SerializedName("weight")
     var weight: Double?,
+    @SerializedName("height")
     var height: Double?,
+    @SerializedName("usia")
+    var usia: Int?
 )
