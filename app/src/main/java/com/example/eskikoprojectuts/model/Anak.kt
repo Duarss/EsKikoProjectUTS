d4c3329394a08cb894ae5c2ec39633243d23e64c
Fransiscus Xaverius Surya Darmawan <s160422003@student.ubaya.ac.id>
2025-10-13 00:46:06 +0700

    up model

--- app/src/main/java/com/example/eskikoprojectuts/model/Anak.kt
+++ app/src/main/java/com/example/eskikoprojectuts/model/Anak.kt
@@ -1,12 +1,7 @@
 package com.example.eskikoprojectuts.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Anak (
-    @SerializedName("weight")
     var weight: Double?,
-    @SerializedName("height")
     var height: Double?,
-    @SerializedName("usia")
     var usia: Int?
 )
