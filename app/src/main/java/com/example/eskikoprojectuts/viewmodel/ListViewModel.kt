9b7acc20bdfd2df37bc16e57a1d537811331c7f8
Fransiscus Xaverius Surya Darmawan <s160422003@student.ubaya.ac.id>
2025-10-13 00:33:37 +0700

    up listviewmodel simpanData

--- app/src/main/java/com/example/eskikoprojectuts/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/eskikoprojectuts/viewmodel/ListViewModel.kt
@@ -3,9 +3,26 @@ package com.example.eskikoprojectuts.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.example.eskikoprojectuts.util.FileHelper
 
 class ListViewModel (application: Application): AndroidViewModel(application) {
     val messageLD = MutableLiveData<String>()
 
+    fun simpanData(berat: String, tinggi: String, usia: String){
+        val fileHelper = FileHelper(getApplication())
 
+        if (berat.isEmpty() || tinggi.isEmpty() || usia.isEmpty()) {
+            messageLD.value = "Semua data wajib diisi"
+            return
+        }
+
+        try{
+            val data = "Usia: $usia tahun | Tinggi: $tinggi cm | Berat: $berat kg"
+            fileHelper.writeToFile(data)
+            messageLD.value = "Data berhasil disimpan"
+        }
+        catch (e: Exception) {
+            messageLD.value = "Data gagal disimpan: ${e.message}"
+        }
+    }
 }
