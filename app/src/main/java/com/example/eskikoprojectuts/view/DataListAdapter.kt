c2c567ea37e671a428dc2a980a11ed3c5ba4f367
Mathew Reynald <mathew.reynald04@gmail.com>
2025-10-15 21:10:33 +0700

    data list fragment dan adapter

--- app/src/main/java/com/example/eskikoprojectuts/view/DataListAdapter.kt
+++ app/src/main/java/com/example/eskikoprojectuts/view/DataListAdapter.kt
@@ -1,6 +1,37 @@
 package com.example.eskikoprojectuts.view
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.eskikoprojectuts.databinding.DataListItemBinding
 import com.example.eskikoprojectuts.model.Anak
 
-class DataListAdapter (val dataList: ArrayList<Anak>) {
+class DataListAdapter (val dataList: ArrayList<Anak>)
+    :RecyclerView.Adapter<DataListAdapter.DataViewHolder>()
+{
+    class DataViewHolder(var binding: DataListItemBinding)
+        : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        val binding = DataListItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return DataViewHolder(binding)
+    }
+
+    override fun getItemCount() = dataList.size
+
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.binding.txtUsia.text = dataList[position].usia.toString()
+        holder.binding.txtHeight.text = dataList[position].height.toString()
+        holder.binding.txtWeight.text = dataList[position].weight.toString()
+
+
+    }
+
+    fun updateDataList(newDataList: ArrayList<Anak>) {
+        dataList.clear()
+        dataList.addAll(newDataList)
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/example/eskikoprojectuts/view/DataListFragment.kt
+++ app/src/main/java/com/example/eskikoprojectuts/view/DataListFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.eskikoprojectuts.R
 import com.example.eskikoprojectuts.databinding.FragmentDataListBinding
-import com.example.eskikoprojectuts.viewmodel.ListViewModel
+import com.example.eskikoprojectuts.viewmodel.DataListViewModel
 
 class DataListFragment : Fragment() {
     private lateinit var binding: FragmentDataListBinding
-    private lateinit var viewModel: ListViewModel
+    private lateinit var viewModel: DataListViewModel
     private val dataListAdapter = DataListAdapter(arrayListOf())
 
     override fun onCreateView(
@@ -24,5 +27,52 @@ class DataListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // init the vm
+        viewModel = ViewModelProvider(this).get(DataListViewModel::class.java)
+        observeViewModel()
+
+        // setup recycler view
+        binding.recViewData.layoutManager = LinearLayoutManager(context)
+        binding.recViewData.adapter = dataListAdapter
+
+        // swipe refresh
+        binding.refreshLayout.setOnRefreshListener {
+            viewModel.refresh()
+            binding.refreshLayout.isRefreshing = false
+        }
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        // obeserve - live data - arraylist student
+        viewModel.dataLD.observe(viewLifecycleOwner, Observer {
+            dataListAdapter.updateDataList(it)
+        })
+
+        // observe - live data - loadingLD
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                // still loading
+                binding.progressLoad.visibility = View.VISIBLE
+                binding.recViewData.visibility = View.INVISIBLE
+
+            } else {
+                // sudah ga loading
+                binding.progressLoad.visibility = View.INVISIBLE
+                binding.recViewData.visibility = View.VISIBLE
+
+            }
+        })
+
+        viewModel.dataLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                // ada error
+                binding.txtError.text = "Something wrong when load data"
+                binding.txtError.visibility = View.VISIBLE
+            } else {
+                binding.txtError.visibility = View.INVISIBLE
+            }
+        })
     }
 }
